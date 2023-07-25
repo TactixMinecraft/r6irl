51710a85636e90f4df43044ff80b3c8fd2c6fad6
TactixMinecraft <133821384+TactixMinecraft@users.noreply.github.com>
2023-07-24 17:08:55 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,43 +1,96 @@
-body {
-  font-family: Arial, sans-serif;
-  text-align: center;
-  margin: 20px;
+// Sample operator data (you can replace this with actual data later)
+const operators = [
+  { name: "Operator 1", ability: "Ability 1", gun: "Gun 1" },
+  { name: "Operator 2", ability: "Ability 2", gun: "Gun 2" },
+  // Add more operators here
+];
+
+const attachments = [
+  "Attachment 1",
+  "Attachment 2",
+  // Add more attachments here
+];
+
+let currentOperatorIndex = 0;
+let currentAttachmentIndex = 0;
+
+// Function to populate the operator dropdown
+function populateOperators() {
+  const operatorsDropdown = document.getElementById("operators");
+
+  operators.forEach((operator) => {
+    const option = document.createElement("option");
+    option.value = operator.name;
+    option.textContent = operator.name;
+    operatorsDropdown.appendChild(option);
+  });
+
+  // Update the operator info for the first operator
+  displayOperatorInfo(currentOperatorIndex);
 }
 
-h1 {
-  color: #007BFF;
+// Function to populate the attachment dropdown
+function populateAttachments() {
+  const attachmentsDropdown = document.getElementById("attachments");
+
+  attachments.forEach((attachment) => {
+    const option = document.createElement("option");
+    option.value = attachment;
+    option.textContent = attachment;
+    attachmentsDropdown.appendChild(option);
+  });
 }
 
-#operatorSelection, #attachmentSelection {
-  display: inline-block;
-  width: 300px;
-  margin: 20px;
+// Function to display operator information
+function displayOperatorInfo(operatorIndex) {
+  const operatorInfoDiv = document.getElementById("operatorInfo");
+
+  // Find the selected operator based on its index
+  const selectedOperator = operators[operatorIndex];
+
+  // Create the operator information content
+  const operatorInfoContent = `
+    <h2>${selectedOperator.name}</h2>
+    <p><strong>Ability:</strong> ${selectedOperator.ability}</p>
+    <p><strong>Gun:</strong> ${selectedOperator.gun}</p>
+  `;
+
+  // Update the operatorInfoDiv with the new content
+  operatorInfoDiv.innerHTML = operatorInfoContent;
 }
 
-select {
-  width: 100%;
-  padding: 5px;
-  margin-top: 5px;
-}
+// Event listeners for operator and attachment selection
+document.addEventListener("DOMContentLoaded", () => {
+  populateOperators();
+  populateAttachments();
 
-.arrow-nav {
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-  padding: 10px;
-  background-color: #007BFF;
-  color: white;
-  border-radius: 5px;
-}
+  // Operator navigation with arrow icons
+  document.getElementById("prevOperator").addEventListener("click", () => {
+    currentOperatorIndex = (currentOperatorIndex - 1 + operators.length) % operators.length;
+    document.getElementById("operators").value = operators[currentOperatorIndex].name;
+    displayOperatorInfo(currentOperatorIndex);
+  });
 
-.arrow-nav i {
-  cursor: pointer;
-}
+  document.getElementById("nextOperator").addEventListener("click", () => {
+    currentOperatorIndex = (currentOperatorIndex + 1) % operators.length;
+    document.getElementById("operators").value = operators[currentOperatorIndex].name;
+    displayOperatorInfo(currentOperatorIndex);
+  });
 
-#operatorInfo {
-  max-width: 500px;
-  margin: 0 auto;
-  padding: 20px;
-  border: 2px solid #007BFF;
-  border-radius: 5px;
-}
+  // Attachment navigation with arrow icons
+  document.getElementById("prevAttachment").addEventListener("click", () => {
+    currentAttachmentIndex = (currentAttachmentIndex - 1 + attachments.length) % attachments.length;
+    document.getElementById("attachments").value = attachments[currentAttachmentIndex];
+  });
+
+  document.getElementById("nextAttachment").addEventListener("click", () => {
+    currentAttachmentIndex = (currentAttachmentIndex + 1) % attachments.length;
+    document.getElementById("attachments").value = attachments[currentAttachmentIndex];
+  });
+
+  // Display operator info when an operator is selected from the dropdown
+  document.getElementById("operators").addEventListener("change", (event) => {
+    currentOperatorIndex = operators.findIndex((operator) => operator.name === event.target.value);
+    displayOperatorInfo(currentOperatorIndex);
+  });
+});
