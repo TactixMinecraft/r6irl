541acc114f23c12052f9b128b144d36055a19931
TactixMinecraft <133821384+TactixMinecraft@users.noreply.github.com>
2023-08-03 09:36:44 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,99 +1,122 @@
-// Sample operator data (you can replace this with actual data later)
-const operators = [
-  { name: "Operator 1", ability: "Ability 1", gun: "Gun 1" },
-  { name: "Operator 2", ability: "Ability 2", gun: "Gun 2" },
-  // Add more operators here
-];
-
-const attachments = [
-  "Attachment 1",
-  "Attachment 2",
-  // Add more attachments here
-];
-
-let currentOperatorIndex = 0;
-let currentAttachmentIndex = 0;
-
-// Function to populate the operator dropdown
-function populateOperators() {
-  const operatorsDropdown = document.getElementById("operators");
-
-  operators.forEach((operator) => {
-    const option = document.createElement("option");
-    option.value = operator.name;
-    option.textContent = operator.name;
-    operatorsDropdown.appendChild(option);
-  });
-
-  // Update the operator info for the first operator
-  displayOperatorInfo(currentOperatorIndex);
+body {
+    background-color: #121212;
+    color: #fff;
+    font-family: Arial, sans-serif;
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
 }
 
-// Function to populate the attachment dropdown
-function populateAttachments() {
-  const attachmentsDropdown = document.getElementById("attachments");
-
-  attachments.forEach((attachment) => {
-    const option = document.createElement("option");
-    option.value = attachment;
-    option.textContent = attachment;
-    attachmentsDropdown.appendChild(option);
-  });
+.container {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex-direction: column;
+    margin: 0 auto;
+    padding: 50px;
+    animation: fadeIn 2s;
 }
 
-// Function to display operator information
-function displayOperatorInfo(operatorIndex) {
-  const operatorInfoDiv = document.getElementById("operatorInfo");
-
-  // Find the selected operator based on its index
-  const selectedOperator = operators[operatorIndex];
-
-  // Create the operator information content
-  const operatorInfoContent = `
-    <h2>${selectedOperator.name}</h2>
-    <p><strong>Ability:</strong> ${selectedOperator.ability}</p>
-    <p><strong>Gun:</strong> ${selectedOperator.gun}</p>
-  `;
-
-  // Update the operatorInfoDiv with the new content
-  operatorInfoDiv.innerHTML = operatorInfoContent;
+@keyframes fadeIn {
+    from {opacity: 0;}
+    to {opacity: 1;}
 }
 
-// Event listeners for operator and attachment selection
-document.addEventListener("DOMContentLoaded", () => {
-  populateOperators();
-  populateAttachments();
+.info {
+    border: 1px solid #444;
+    border-radius: 5px;
+    padding: 20px;
+    margin: 20px;
+    width: 60%;
+    text-align: center;
+    background-color: #202020;
+    transition: background-color 0.5s;
+}
 
-  const operatorsDropdown = document.getElementById("operators");
-  const attachmentsDropdown = document.getElementById("attachments");
+.info h2 {
+    font-size: 2em;
+    margin-bottom: 10px;
+}
 
-  // Operator navigation with arrow icons
-  document.getElementById("prevOperator").addEventListener("click", () => {
-    currentOperatorIndex = (currentOperatorIndex - 1 + operators.length) % operators.length;
-    operatorsDropdown.value = operators[currentOperatorIndex].name;
-    displayOperatorInfo(currentOperatorIndex);
-  });
+.info p {
+    font-size: 1.2em;
+}
 
-  document.getElementById("nextOperator").addEventListener("click", () => {
-    currentOperatorIndex = (currentOperatorIndex + 1) % operators.length;
-    operatorsDropdown.value = operators[currentOperatorIndex].name;
-    displayOperatorInfo(currentOperatorIndex);
-  });
+button {
+    background-color: #1abc9c;
+    border: none;
+    color: white;
+    padding: 15px 32px;
+    text-align: center;
+    text-decoration: none;
+    display: inline-block;
+    font-size: 16px;
+    margin: 4px 2px;
+    cursor: pointer;
+    border-radius: 5px;
+    transition: background-color 0.3s ease, transform 0.3s ease;
+}
 
-  // Attachment navigation with arrow icons
-  document.getElementById("prevAttachment").addEventListener("click", () => {
-    currentAttachmentIndex = (currentAttachmentIndex - 1 + attachments.length) % attachments.length;
-    attachmentsDropdown.value = attachments[currentAttachmentIndex];
-  });
+button:active {
+    transform: scale(0.95);
+}
 
-  document.getElementById("nextAttachment").addEventListener("click", () => {
-    currentAttachmentIndex = (currentAttachmentIndex + 1) % attachments.length;
-    attachmentsDropdown.value = attachments[currentAttachmentIndex];
-  });
+button:hover {
+    background-color: #16a085;
+}
 
-  // Display operator info when an operator is selected from the dropdown
-  operatorsDropdown.addEventListener("change", (event) => {
-    currentOperatorIndex = operators.findIndex((operator) => operator.name === event.target.value);
-    displayOperatorInfo(currentOperatorIndex);
-  });
-});
+.attachments div {
+    margin-bottom: 15px;
+}
+
+.attachments h3 {
+    margin-bottom: 5px;
+}
+
+.attachments select {
+    width: 100%;
+    padding: 10px;
+    border-radius: 5px;
+    border: 1px solid #444;
+    background-color: #202020;
+    color: #fff;
+}
+
+.reset-button {
+    background-color: #e74c3c;
+}
+
+.reset-button:hover {
+    background-color: #c0392b;
+}
+
+@media only screen and (max-width: 600px) {
+    .container {
+        padding: 20px;
+    }
+
+    .info {
+        width: 100%;
+    }
+
+    .info h2 {
+        font-size: 1.5em;
+    }
+
+    .info p {
+        font-size: 1em;
+    }
+
+    button {
+        padding: 10px 20px;
+        font-size: 14px;
+    }
+
+    .attachments h3 {
+        margin-bottom: 5px;
+    }
+
+    .attachments select {
+        padding: 5px;
+    }
+}
