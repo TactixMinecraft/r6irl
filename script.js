a9b55caf7afd7aaed7cca0b117aad4016a19d886
TactixMinecraft <133821384+TactixMinecraft@users.noreply.github.com>
2023-08-04 18:04:09 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,18 +1,16 @@
-// Operators and weapons data
 var operators = [
     {name: 'Flash', role: 'Defense'},
     {name: 'Ring', role: 'Attacker'},
     {name: 'Alarm', role: 'Defense'},
-    {name: 'Ghost', role: 'Attacker'}
+    {name: 'Ghost', role: 'Unknown'}
 ];
 
 var weapons = [
-    {name: 'Retaliator', type: 'Rifle', attachments: {sight: ['Medium', 'Sniper', 'Bullet', 'Night Vision', 'Handle'], barrel: ['Tactical', 'Short', 'Long', 'Handle', 'Stand'], stock: ['Tactical', 'Long', 'Utility']}},
-    {name: 'Recon MKlll', type: 'SMG', attachments: {sight: ['Medium', 'Sniper', 'Bullet', 'Night Vision', 'Handle'], barrel: ['Tactical', 'Short', 'Long', 'Handle', 'Stand'], stock: ['Tactical', 'Long', 'Utility']}},
-    {name: 'Strongarm', type: 'Handgun', attachments: {sight: ['Medium', 'Sniper', 'Bullet', 'Night Vision', 'Handle'], barrel: ['Tactical', 'Short', 'Long', 'Handle', 'Stand'], stock: ['Tactical', 'Long', 'Utility']}}
+    {name: 'Retaliator', type: 'Assault Rifle', attachments: {sight: ['Medium', 'Sniper', 'Bullet', 'Night vision', 'Handle'], barrel: ['Tactical', 'Short', 'Long', 'Handle', 'Stand'], stock: ['Tactical', 'Long', 'Utility']}},
+    {name: 'Recon MKlll', type: 'SMG', attachments: {sight: ['Medium', 'Sniper', 'Bullet', 'Night vision', 'Handle'], barrel: ['Tactical', 'Short', 'Long', 'Handle', 'Stand'], stock: ['Tactical', 'Long', 'Utility']}},
+    {name: 'Strongarm', type: 'Handgun', attachments: {sight: ['Medium', 'Sniper', 'Bullet', 'Night vision', 'Handle'], barrel: ['Tactical', 'Short', 'Long', 'Handle', 'Stand'], stock: ['Tactical', 'Long', 'Utility']}}
 ];
 
-// Initialize
 var currentOperatorIndex = 0;
 var currentWeaponIndex = 0;
 
@@ -24,90 +22,58 @@ var prevOperatorButton = document.getElementById('prevOperatorButton');
 var nextOperatorButton = document.getElementById('nextOperatorButton');
 var prevWeaponButton = document.getElementById('prevWeaponButton');
 var nextWeaponButton = document.getElementById('nextWeaponButton');
-var resetButton = document.getElementById('resetButton');
 
-// Functions to show the current operator and weapon
-function showOperator() {
-    var operatorInfo = document.getElementById('operatorInfo');
-    var operator = operators[currentOperatorIndex];
-
-    operatorInfo.innerHTML = `
-        <h2>${operator.name}</h2>
-        <p>${operator.role}</p>
-    `;
-}
-
-function showWeapon() {
-    var weaponInfo = document.getElementById('weaponInfo');
-    var weapon = weapons[currentWeaponIndex];
-
-    weaponInfo.innerHTML = `
-        <h2>${weapon.name}</h2>
-        <p>${weapon.type}</p>
-    `;
-}
-
-function showAttachments() {
-    var attachments = weapons[currentWeaponIndex].attachments;
-    var attachmentsHTML = '';
-
-    for (var attachmentType in attachments) {
-        attachmentsHTML += `
-            <div>
-                <h3>${attachmentType}</h3>
-                <select id="${attachmentType}">
-                    ${attachments[attachmentType].map(attachment => `<option>${attachment}</option>`).join('')}
-                </select>
-            </div>
-        `;
-    }
-
-    attachmentsSection.innerHTML = attachmentsHTML;
-}
-
-// Event listeners for the buttons
 prevOperatorButton.addEventListener('click', function() {
     currentOperatorIndex--;
-    if (currentOperatorIndex < 0) currentOperatorIndex = operators.length - 1;
+    if(currentOperatorIndex < 0) currentOperatorIndex = operators.length - 1;
     showOperator();
 });
 
 nextOperatorButton.addEventListener('click', function() {
     currentOperatorIndex++;
-    if (currentOperatorIndex >= operators.length) currentOperatorIndex = 0;
+    if(currentOperatorIndex >= operators.length) currentOperatorIndex = 0;
     showOperator();
 });
 
 prevWeaponButton.addEventListener('click', function() {
     currentWeaponIndex--;
-    if (currentWeaponIndex < 0) currentWeaponIndex = weapons.length - 1;
+    if(currentWeaponIndex < 0) currentWeaponIndex = weapons.length - 1;
     showWeapon();
-    showAttachments(); // Update attachments when weapon changes
 });
 
 nextWeaponButton.addEventListener('click', function() {
     currentWeaponIndex++;
-    if (currentWeaponIndex >= weapons.length) currentWeaponIndex = 0;
+    if(currentWeaponIndex >= weapons.length) currentWeaponIndex = 0;
     showWeapon();
-    showAttachments(); // Update attachments when weapon changes
 });
 
-resetButton.addEventListener('click', function() {
-    // Reset the current operator and weapon indices
-    currentOperatorIndex = 0;
-    currentWeaponIndex = 0;
+function showOperator() {
+    var operator = operators[currentOperatorIndex];
+    operatorSection.innerHTML = '<h2>' + operator.name + '</h2><p>' + operator.role + '</p>';
+}
 
-    // Show the operator and weapon sections
-    operatorSection.style.display = 'block';
-    weaponSection.style.display = 'block';
+function showWeapon() {
+    var weapon = weapons[currentWeaponIndex];
+    weaponSection.innerHTML = '<h2>' + weapon.name + '</h2><p>' + weapon.type + '</p>';
 
-    // Reset the data in the operator and weapon sections
-    showOperator();
-    showWeapon();
-    showAttachments();
-});
+    attachmentsSection.innerHTML = '';
+    for(var attachmentType in weapon.attachments) {
+        var attachmentDiv = document.createElement('div');
+        var attachmentLabel = document.createElement('h3');
+        attachmentLabel.textContent = attachmentType;
+        attachmentDiv.appendChild(attachmentLabel);
 
-// Display the first operator and weapon when the page loads
+        var attachmentSelect = document.createElement('select');
+        weapon.attachments[attachmentType].forEach(function(attachmentOption) {
+            var option = document.createElement('option');
+            option.text = attachmentOption;
+            attachmentSelect.add(option);
+        });
+
+        attachmentDiv.appendChild(attachmentSelect);
+        attachmentsSection.appendChild(attachmentDiv);
+    }
+}
+
 showOperator();
 showWeapon();
-showAttachments();
